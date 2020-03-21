8b3b992040f8db54baf4bb6b5735ac1e6d43359a
Yang <stardussky@gmail.com>
2020-03-21 15:51:14 +0800

    init

--- template/vue.config.js
+++ template/vue.config.js
@@ -7,5 +7,17 @@ module.exports = {
     },
     disableHostCheck: true
   },
-  lintOnSave: process.env.NODE_ENV !== 'production'
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  chainWebpack: config => {
+    const oneOfsMap = config.module.rule('scss').oneOfs.store
+    oneOfsMap.forEach(item => {
+      item
+        .use('sass-resources-loader')
+        .loader('sass-resources-loader')
+        .options({
+          resources: ['./src/style/_main.scss']
+        })
+        .end()
+    })
+  }
 }
