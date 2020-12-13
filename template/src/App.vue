d32a77af8eaf7760d19ca5b2c68002088de5f8dd
Evan <evan@blockstudio.tw>
2020-12-13 16:54:10 +0800

    [update] import global css

--- template/src/App.vue
+++ template/src/App.vue
@@ -86,21 +86,22 @@ export default {
 </script>
 
 <style lang="scss">
-    #app {
-        text-align: center;
+@import '@/style/_main.scss';
+#app {
+    text-align: center;
+    color: #2c3e50;
+}
+
+#nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
         color: #2c3e50;
-    }
 
-    #nav {
-        padding: 30px;
-
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
-            }
+        &.router-link-exact-active {
+            color: #42b983;
         }
     }
+}
 </style>
